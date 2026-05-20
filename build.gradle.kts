aa224d7f7e30fabb8f49a7e6cba6cfb781c45fd5
RyabukhinaMariya <1411mashakrasnodar@gmail.ru>
2026-05-20 21:30:56 +0300

    complete 2 hw

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,9 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation("io.mockk:mockk:1.13.10")
+    testImplementation(kotlin("test"))
 }
 
 tasks.jar {
