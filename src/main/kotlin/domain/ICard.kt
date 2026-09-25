3640cba7cec72bbe96fe3a9f5bc5284cd59294f4
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2026-09-25 17:32:51 +0300

    Delete src directory

--- src/main/kotlin/domain/ICard.kt
+++
@@ -1,5 +0,0 @@
-package domain
-
-interface ICard {
-    val name: String
-}
--- src/main/kotlin/domain/ICharacter.kt
+++
@@ -1,8 +0,0 @@
-package domain
-import engine.QuarterPool
-
-interface ICharacter: ICard {
-    val rank: Int
-    var isKilled: Boolean
-    fun ability(activePlayer: Player?, players: MutableList<IPlayer>, quarterPool: QuarterPool)
-}
